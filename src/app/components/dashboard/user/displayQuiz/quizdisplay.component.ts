4d69258bf45cee922bdb84defebc4c7ed610c5de
Tejaskolhe <kolhetejas12@gmail.com>
2022-10-06 11:34:56 +0530

    header commit

--- src/app/components/dashboard/user/displayQuiz/quizdisplay.component.ts
+++ src/app/components/dashboard/user/displayQuiz/quizdisplay.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
@@ -14,20 +14,19 @@ export class QuizdisplayComponent implements OnInit {
   constructor(private cookie:CookieService, private router:Router,private http:HttpClient) { }
  uservalue:any
   ngOnInit(): void {
-    this.http.get(environment.userUrl+"exam-portal/token/validate",{ observe: 'response', withCredentials:true,responseType:'text'})
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'jwt': this.cookie.get('jwt')
+    });
 
-    .subscribe((data:any)=>{
-    this.uservalue=data
-    if(this.uservalue.body!="user")
-     {
-      alert("You are not LoggedIn")
-      this.router.navigate([''])
-     }
-
-    },(error:any)=>{
-      alert("You are not LoggedIn")
-      this.router.navigate([''])
-    })
+    this.http.get(environment.userUrl + 'exam-portal/token/validate', { headers: headers, withCredentials: true })
+      .subscribe((data: any) => {
+        this.uservalue = data
+        if (this.uservalue != 'user') {
+          alert("You are not LoggedIn")
+          this.router.navigate([''])
+        }
+      })
   }
 
   logout() {
